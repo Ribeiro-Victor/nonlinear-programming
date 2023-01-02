4fcb6cae0ad7105602df4c42e07bf9ef1348dd0a
Your Name <leonardongc@gmail.com>
2023-01-02 18:21:58 -0300

    método de newton feito(com um macete no pontos de não concavidade)

--- tests.py
+++ tests.py
@@ -1,16 +1,21 @@
 from optmengine import *
 from random import randint
 
-def getRandom():
-    nom = 1.0*randint(1,999999999)
-    denom = 1.0*randint(1,999999999)
-    return nom/denom
-
 def getPoint():
-    #point = (getRandom(), getRandom())
-    #print(f'get the point : {point}')
+    return 1.0/randint(1,10) + 1.0*randint(0,10), 1.0/randint(1,10) + 1.0*randint(0,10)
 
-    return randint(1,100),randint(1,100)
+
+def getCANTO():
+    return 1.0/randint(1,100), 1.0/randint(1,100)
+
+def getPAREDE1():
+    return 1.0*randint(1,100), 1.0/randint(1,100)
+
+def getPAREDE2():
+    return 1.0/randint(1,100), 1.0*randint(1,100)
+
+def getPLANALTO():
+    return 1.0*randint(1,500),1.0*randint(1,500)
 
 
 print("\n TESTANDO \n")
@@ -18,6 +23,7 @@ print("\n TESTANDO \n")
 
 pontoCANTO = {'startingPoint': (0.5,0.5), 'iterations': 0, 'stepSizeCalls': 0, 'currentPoint': (.5,.5), 'currentValue': funcValue(.5,.5), 'residual': 0}
 
+canto, parede1, parede2, planalto = getCANTO(), getPAREDE1(), getPAREDE2(), getPLANALTO()
 #testando evaluação:
 print("\n TESTANDO FUNÇÃO\n")
 print(funcValue(1000,200))
@@ -35,6 +41,17 @@ print(gradValue(5,10))
 print(gradValue(.1,1))
 print(gradValue(.999999,1))
 
+#testando hessiana
+print("\nTESTANDO HESSIANA\n\n")
+evCANTO = eigenValues(hessianValue(canto[0],canto[1]))
+evPAREDE1 = eigenValues(hessianValue(parede1[0],parede1[1]))
+evPAREDE2 = eigenValues(hessianValue(parede2[0],parede2[1]))
+evPLANALTO = eigenValues(hessianValue(planalto[0],planalto[1]))
+
+checkConvexity(evCANTO[0],evCANTO[1])
+checkConvexity(evPAREDE1[0],evPAREDE1[1])
+checkConvexity(evPAREDE2[0],evPAREDE2[1])
+checkConvexity(evPLANALTO[0],evPLANALTO[1])
 
 
 
@@ -70,4 +87,32 @@ print("\n")
 print(gradientMethod(getPoint()))
 #testando tabela:
 print("\n"*3)
-simulate([getPoint() for i in range(10)],gradientMethod,"Tabela 1 Método Gradiente")
+#simulate([getPoint() for i in range(10)],gradientMethod,"Tabela 1 Método Gradiente")
+
+
+
+
+
+
+
+simulate([canto,parede1,parede2,planalto,getPoint()],gradientMethod,"TABELA 1 MÉTODO GRADIENTE")
+
+simulate([canto,parede1,parede2,planalto,getPoint()],newtonMethod,"TABELA 2 MÉTODO DE NEWTON")
+
+simulate([canto,parede1,parede2,planalto,getPoint()],mockMethod,"TABELA 3 MÉTODO DAS DIREÇÕES FIXAS")
+
+"""
+while(True):
+    testPoint = getPLANALTO()
+    gradientValue = gradValue(testPoint[0],testPoint[1])
+    hessian = hessianValue(testPoint[0],testPoint[1])
+    evs = eigenValues(hessian)
+    inverted = np.linalg.inv(hessian)
+    descendValue = np.dot(inverted, gradientValue)
+    print(descendValue)
+"""
+
+  
+#print(algoGenSearch((.5,.5)))
+
+#print(algoGenSearch((.5,.5)))
